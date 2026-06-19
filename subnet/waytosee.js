fabe3c4099a49784433d837ee11d49fde70f719a
Evyatar Friedmann <deepspace1@icloud.com>
2026-06-19 11:42:38 +0300

    Save waytosee.js

--- subnet/waytosee.js
+++ subnet/waytosee.js
@@ -1,1 +1,16 @@
-alert("𝄢");
+document.addEventListener("DOMContentLoaded", () => {
+  const focusButton = document.querySelector(".focus-toggle");
+  if (!focusButton) return;
+
+  function updateFocusButton() {
+    const isFocus = document.body.classList.contains("focus-mode");
+    focusButton.innerHTML = isFocus ? "𝄢" : "תודעת לימוד";
+  }
+
+  focusButton.onclick = () => {
+    document.body.classList.toggle("focus-mode");
+    updateFocusButton();
+  };
+
+  updateFocusButton();
+});ד
